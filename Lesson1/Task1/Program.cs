a2cc832e56291df7c52bdda71d9d362fcf1ddbbd
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-16 21:00:49 +0300

    add Lesson1:Task1

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.WriteLine("1 Hello world!");
+// Console.WriteLine("2 Hello world!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.54;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
