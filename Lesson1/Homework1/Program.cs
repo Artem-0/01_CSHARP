767ae091eea25f5fff176d5051b69403b3f3ade2
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-16 21:02:22 +0300

    add Lesson1:Homework1

---
+++ Lesson1/Homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿//int a = 100;
+//int b = 1;
+//int c = 2;
+//int d = 5;
+
+//int produkt = a * b;
+//float sum = c + d;
+//float ans = produkt / sum;
+//Console.WriteLine(ans);
+
+int a = 100;
+int b = 1;
+int c = 2;
+double d = 5;
+
+double f = (a * b) / (c + d);
+Console.WriteLine(f);
