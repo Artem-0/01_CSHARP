8f79a6f7fb3694c4ef8c8e6cc465d85e0a624752
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-21 14:56:54 +0300

    add lesson2 again

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n]; // или int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
