b4b62a839020ff5fee40ee9fb4680010a80f3c1f
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-21 14:57:31 +0300

    add lesson2:tsk3, 4

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] arr = { 4, 3, 8, 16, 9, 2 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// int n = 10;
+int[] arr = { 2, -8, -1, 6, 3 };
+
+int i = 0;
+int max = arr[i];
+
+while (i < arr.Length)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine($"{max}");
+
+max = arr[0];
+for(int j = 0; j < arr.Length; j++)
+{
+     if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+System.Console.WriteLine($"{max}");
+
+max = arr[0];
+foreach(int e in arr)
+{
+     if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine($"{max}");
