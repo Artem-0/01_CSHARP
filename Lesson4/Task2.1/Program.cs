2cd37750b488a851cc820789986b949ef8fcd6e6
Artem-0 <artiem.ruchii.99@mail.ru>
2024-12-25 22:14:14 +0300

    Lesson4 add Task2.1

---
+++ Lesson4/Task2.1/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ShowInterest(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int value = matrix[i, j];
+                int sum = 0;
+                while (value > 0)
+                {
+                    sum = sum + value % 10;
+                    value = value / 10;
+                    if (sum % 2 == 0)
+                    {
+                        System.Console.Write(matrix[i, j] + " ");
+                    }
+                }
+            }
+        }
+}
+
+
+int[,] matrix = CreateMatrix(4, 5);
+System.Console.WriteLine("Матрица:");
+ShowMatrix(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine("Чётные элементы матрицы :");
+ShowInterest(matrix);
